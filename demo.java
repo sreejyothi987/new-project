ab09059704cd7fe4b8d766b2f859e56016ac3dd6
sreejyothi987 <97618780+sreejyothi987@users.noreply.github.com>
2022-02-24 18:25:02 +0530

    demo file created

---
+++ demo.java
@@ -0,0 +1,19 @@
+public class Chartostring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//method1
+		char c='G';
+		String s=""+c;
+		System.out.println(s);
+		System.out.println(s.length());
+		//method2
+		String s1=Character.toString(c);
+		System.out.println(s1);
+
+
+		
+
+	}
+
+}
